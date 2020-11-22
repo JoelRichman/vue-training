07faec180e0ac67e47baf4c63e2b96accda3e502
Joel Richman <joel@dbconcepts.net>
2020-11-22 12:23:12 -0700

    Add filter and computed property.

--- app/song-list.component.js
+++ app/song-list.component.js
@@ -6,7 +6,8 @@ app.component('app-song-list', {
         { id: 1, title: 'Pride in the Name of Love', artist: 'U2', length: '3' },
         { id: 2, title: 'Welcome to the Jungle', artist: 'Guns n Roses', length: '3.5' }
       ],
-      selectedSongId: 0
+      selectedSongId: 0,
+      filter: ''
     }
   },
   methods: {
@@ -14,7 +15,22 @@ app.component('app-song-list', {
       this.selectedSongId = song.id;
     }
   },
+  computed: {
+    filteredSongs() {
+      if (this.filter) {
+        const lcFilter = this.filter.toLowerCase();
+        return this.songs.filter(x => {
+          return x.title.toLowerCase().indexOf(lcFilter) > -1
+        });
+      }
+
+      return this.songs;
+    }
+  },
   template: `
+    <div class="filter-container">
+      Filter: <input type="text" v-model="filter" />  {{ filter }}
+    </div>
     <table class="table">
       <thead>
         <tr>
@@ -24,13 +40,12 @@ app.component('app-song-list', {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="song in songs" :key="id" @click="selectSong(song)" :class="{ active: song.id === selectedSongId }">
+        <tr v-for="song in filteredSongs" :key="id" @click="selectSong(song)" :class="{ active: song.id === selectedSongId }">
           <td>{{ song.title }}</td>
           <td>{{ song.artist }}</td>
           <td>{{ song.length }}</td>
         </tr>
       </tbody>
     </table>
-    {{ selectedSongId }}
   `
 })
